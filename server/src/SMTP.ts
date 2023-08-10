7d52a223fd9f33ed4d6e37d16d31ad2c280a3d07
Dominic Galiano <dgaliano@utexas.edu>
2023-08-10 17:50:51 -0400

    fixed syntax error

--- server/src/SMTP.ts
+++ server/src/SMTP.ts
@@ -1,33 +1,37 @@
 import Mail from "nodemailer/lib/mailer";
 import * as nodemailer from "nodemailer";
 import { SendMailOptions, SentMessageInfo } from "nodemailer";
-import { inflateRaw } from "zlib";
-const nodemailer = require("nodemailer");
+import { IServerInfo } from "./serverInfo";
 
 export class Worker {
   private static serverInfo: IServerInfo;
   constructor(inServerInfo: IServerInfo) {
     Worker.serverInfo = inServerInfo;
   }
-  
+
   /**
    * Send a message
-   * 
+   *
    * @param inOptions An object containing to, from, subject, and text properties
    * @returns A promise that eventually resolves to a string
    */
   public sendMessage(inOptions: SendMailOptions): Promise<string> {
     return new Promise((inResolve, inReject) => {
-      const transport: Mail = nodemailer.createTransport(Worker.serverInfo.smtp);
-      transport.sendMail(inOptions, (inError: Error | null, inInfo: SentMessageInfo) => {
-        if (inError) {
-          console.log("SMTP.Worker.sendMessage(): Error", inError);
-          inReject(inError);
-        } else {
-          console.log("SMTP.Worker.sendMessage(): Ok", inInfo);
-          inResolve();
+      const transport: Mail = nodemailer.createTransport(
+        Worker.serverInfo.smtp
+      );
+      transport.sendMail(
+        inOptions,
+        (inError: Error | null, inInfo: SentMessageInfo) => {
+          if (inError) {
+            console.log("SMTP.Worker.sendMessage(): Error", inError);
+            inReject(inError);
+          } else {
+            console.log("SMTP.Worker.sendMessage(): Ok", inInfo);
+            inResolve();
+          }
         }
-      });
-    }
+      );
+    });
   }
 }
