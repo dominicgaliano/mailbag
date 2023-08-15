debc9ed5945c21f489aa27c45e30a56ec3dff47e
Dominic Galiano <dgaliano@utexas.edu>
2023-08-15 15:50:45 -0400

    partial state.ts work

--- client/src/utils/state.ts
+++ client/src/utils/state.ts
@@ -1,5 +1,10 @@
 import { useState } from "react";
 
+import * as Contacts from "./Contacts";
+import { config } from "./config";
+import * as IMAP from "./IMAP";
+import * as SMTP from "./SMTP";
+
 export default function createState() {
   const [state, setState] = useState({
     pleaseWaitVisible: false,
@@ -19,12 +24,84 @@ export default function createState() {
     contactEmail: null,
   });
 
-  const showHidePleaseWait = (inVisible: boolean) => {
+  /**
+   * Toggle the please wait modal
+   * @param inVisible boolean true to show, false to hide
+   */
+  const showHidePleaseWait = function (inVisible: boolean): void {
     setState((prevState) => ({
       ...prevState,
       pleaseWaitVisible: inVisible,
     }));
   };
 
-  return { ...state, showHidePleaseWait };
+  /**
+   * Show contact modal
+   * @param inId contact id
+   * @param inName contact name
+   * @param inEmail contact email
+   */
+  const showContact = function (
+    inId: string,
+    inName: string,
+    inEmail: string
+  ): void {
+    setState((prevState) => ({
+      ...prevState,
+      contactID: inId,
+      contactName: inName,
+      contactEmail: inEmail,
+    }));
+  };
+
+  /**
+   * Show add contact modal
+   */
+  const showAddContact = function (): void {
+    setState((prevState) => ({
+      ...prevState,
+      currentView: "addContact",
+    }));
+  };
+
+  /**
+   * Show message modal
+   * @param inMessage
+   * @returns
+   */
+  const showMessage = function (inMessage: IMAP.IMessage): Promise<void> {
+    // show please wait modal
+    showHidePleaseWait(true);
+
+    // get message content
+    imapWorker: IMAP.Worker = new IMAP.Worker();
+    const messageBody: string = imapWorker
+      .getBody
+      // TODO: fill function body
+      ();
+
+    // hide please wait modal
+    setState((prevState) => ({
+      ...prevState,
+      messageID: inMessage.id,
+      messageDate: inMessage.date,
+      messageFrom: inMessage.from,
+      messageTo: inMessage.to,
+      messageSubject: inMessage.subject,
+      messageBody: messageBody,
+    }));
+  };
+
+  /**
+   * Add mailbox to list
+   * @param inMailbox mailbox to add
+   */
+  const addMailboxToList = function (inMailbox: IMAP.IMailbox): void {
+    setState((prevState) => ({
+      ...prevState,
+      mailboxes: [...prevState.mailboxes, inMailbox],
+    }));
+  };
+
+  return { ...state, showHidePleaseWait, addMailboxToList };
 }
