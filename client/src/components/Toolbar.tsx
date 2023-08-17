98c1cb2832deabca757cf2abe5607d29c40b7b74
Dominic Galiano <dgaliano@utexas.edu>
2023-08-17 17:07:49 -0400

    toolbar buttons

--- client/src/components/Toolbar.tsx
+++ client/src/components/Toolbar.tsx
@@ -1,9 +1,35 @@
 import { State } from "../utils/state";
+import { Button } from "@mui/material";
 
 type Props = {
   state: State;
 };
 
 export default function Toolbar({ state }: Props) {
-  return <>Toolbar</>;
+  return (
+    <>
+      <Button
+        variant="contained"
+        color="primary"
+        size="small"
+        style={{ marginRight: 10 }}
+        onClick={() => {
+          // TODO: Implement, need to implement more state methods
+        }}
+      >
+        New Message
+      </Button>
+      <Button
+        variant="contained"
+        color="primary"
+        size="small"
+        style={{ marginRight: 10 }}
+        onClick={() => {
+          // TODO: Implement, need to implement more state methods
+        }}
+      >
+        New Contact
+      </Button>
+    </>
+  );
 }
