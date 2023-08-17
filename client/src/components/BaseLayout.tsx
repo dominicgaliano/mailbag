eed038418233e1001e35cfc66abfbd580248a2f5
Dominic Galiano <dgaliano@utexas.edu>
2023-08-17 13:57:52 -0400

    loading dialog

--- client/src/components/BaseLayout.tsx
+++ client/src/components/BaseLayout.tsx
@@ -1,14 +1,26 @@
 import { useEffect } from "react";
 import { createState, NewMailType } from "../utils/state";
-import { Dialog, DialogTitle } from "@mui/material";
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+} from "@mui/material";
 
 export default function BaseLayout() {
   const state = createState();
+
+  // useEffect(() => {
+  //   state.showHidePleaseWait(true);
+  // }, []);
 
   return (
     <div className="appContainer">
       <Dialog open={state.pleaseWaitVisible} transitionDuration={0}>
         <DialogTitle style={{ textAlign: "center" }}>Please Wait</DialogTitle>
+        <DialogContent>
+          <DialogContentText>...Contacting Server...</DialogContentText>
+        </DialogContent>
       </Dialog>
     </div>
   );
