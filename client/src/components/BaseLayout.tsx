18d59ef920c24e4e1ca7946add8ad240b6022fe7
Dominic Galiano <dgaliano@utexas.edu>
2023-08-16 21:46:29 -0400

    added dialog on load

--- client/src/components/BaseLayout.tsx
+++ client/src/components/BaseLayout.tsx
@@ -1,7 +1,15 @@
+import { useEffect } from "react";
 import { createState, NewMailType } from "../utils/state";
+import { Dialog, DialogTitle } from "@mui/material";
 
 export default function BaseLayout() {
   const state = createState();
 
-  return <div className="appContainer"></div>;
+  return (
+    <div className="appContainer">
+      <Dialog open={state.pleaseWaitVisible} transitionDuration={0}>
+        <DialogTitle style={{ textAlign: "center" }}>Please Wait</DialogTitle>
+      </Dialog>
+    </div>
+  );
 }
