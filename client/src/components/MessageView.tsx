bd27ad82df355a08a6e909ff31186fcd05afa28b
Dominic Galiano <dgaliano@utexas.edu>
2023-08-20 11:51:37 -0400

    initial message view components

--- client/src/components/MessageView.tsx
+++ client/src/components/MessageView.tsx
@@ -1,9 +1,23 @@
-import { State } from "../utils/state";
+import { State, CurrentView } from "../utils/state";
+import { InputBase } from "@mui/material";
 
 type Props = {
   state: State;
 };
 
 export default function MessageView({ state }: Props) {
-  return <>MessageView</>;
+  return (
+    <form>
+      {/* Reading message */}
+      {state.currentView === CurrentView.message && (
+        <InputBase
+          defaultValue={`ID ${state.messageID}`}
+          margin="dense"
+          disabled={true}
+          fullWidth={true}
+          className="message-info-field"
+        />
+      )}
+    </form>
+  );
 }
