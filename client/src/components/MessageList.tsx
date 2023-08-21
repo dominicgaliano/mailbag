23f31bf2e06b962a0a7f9520c2c7bcd113c7fef6
Dominic Galiano <dgaliano@utexas.edu>
2023-08-20 23:08:24 -0400

    finished message list

--- client/src/components/MessageList.tsx
+++ client/src/components/MessageList.tsx
@@ -1,19 +1,46 @@
 import { State } from "../utils/state";
+import {
+  Table,
+  TableHead,
+  TableCell,
+  TableRow,
+  TableBody,
+} from "@mui/material";
 
 type Props = {
   state: State;
 };
 
 export default function MessageList({ state }: Props) {
+  // return (
+  //   <ul>
+  //     {state.messages.map((message) => {
+  //       return (
+  //         <li key={message.id} onClick={() => state.showMessage(message)}>
+  //           {message.subject} From: {message.from}
+  //         </li>
+  //       );
+  //     })}
+  //   </ul>
+  // );
   return (
-    <ul>
-      {state.messages.map((message) => {
-        return (
-          <li key={message.id} onClick={() => state.showMessage(message)}>
-            {message.subject} From: {message.from}
-          </li>
-        );
-      })}
-    </ul>
+    <Table stickyHeader padding="none">
+      <TableHead>
+        <TableRow>
+          <TableCell style={{ width: 120 }}>Date</TableCell>
+          <TableCell style={{ width: 300 }}>From</TableCell>
+          <TableCell>Subject</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {state.messages.map((message) => (
+          <TableRow key={message.id} onClick={() => state.showMessage(message)}>
+            <TableCell>{new Date(message.date).toLocaleDateString()}</TableCell>
+            <TableCell>{message.from}</TableCell>
+            <TableCell>{message.subject}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
   );
 }
